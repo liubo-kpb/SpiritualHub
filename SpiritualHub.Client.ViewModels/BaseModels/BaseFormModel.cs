f7f71b7792623e1c765d153b7179c1b22d540c25
Lyubomir Toshev <lubomirtoshev96@gmail.com>
2023-10-04 12:52:55 +0300

    Create Generic "Base" View Models.

---
+++ SpiritualHub.Client.ViewModels/BaseModels/BaseFormModel.cs
@@ -0,0 +1,34 @@
+﻿namespace SpiritualHub.Client.ViewModels.BaseModels;
+
+using System.ComponentModel.DataAnnotations;
+
+using Author;
+using Category;
+using Publisher;
+
+public class BaseFormModel
+{
+    public BaseFormModel()
+    {
+        this.Categories = new HashSet<CategoryServiceModel>();
+        this.Authors = new HashSet<AuthorInfoViewModel>();
+        this.Publishers = new HashSet<PublisherInfoViewModel>();
+    }
+
+    public string? Id { get; set; } = null!;
+
+    [Display(Name = "Choose Category:")]
+    public int CategoryId { get; set; }
+
+    [Display(Name = "Choose Author:")]
+    public string AuthorId { get; set; } = null!;
+
+    [Display(Name = "Choose Publisher:")]
+    public string PublisherId { get; set; } = null!;
+
+    public IEnumerable<CategoryServiceModel> Categories { get; set; }
+
+    public IEnumerable<AuthorInfoViewModel> Authors { get; set; }
+
+    public IEnumerable<PublisherInfoViewModel> Publishers { get; set; }
+}
---
+++ SpiritualHub.Client.ViewModels/BaseModels/BaseQueryModel.cs
@@ -0,0 +1,40 @@
+﻿namespace SpiritualHub.Client.ViewModels.BaseModels;
+
+using System.ComponentModel.DataAnnotations;
+
+
+using static Common.GeneralApplicationConstants;
+
+public class BaseQueryModel<TViewModel, TSortingEnum>
+    where TViewModel : class
+    where TSortingEnum : Enum
+{
+    public BaseQueryModel()
+    {
+        this.Categories = new HashSet<string>();
+        this.Entities = new HashSet<TViewModel>();
+
+        CurrentPage = DefaultPage;
+        EntitiesPerPage = EntitiesPerPageConstant;
+    }
+
+    [Display(Name = "Category")]
+    public string CategoryName { get; set; } = null!;
+
+    [Display(Name = "Search by text")]
+    public string SearchTerm { get; set; } = null!;
+
+    [Display(Name = "Sort by")]
+    public TSortingEnum? SortingOption { get; set; }
+
+    public int CurrentPage { get; set; }
+
+    [Display(Name = "Show on Page")]
+    public int EntitiesPerPage { get; set; }
+
+    public int TotalEntitesCount { get; set; }
+
+    public IEnumerable<string> Categories { get; set; }
+
+    public IEnumerable<TViewModel> Entities { get; set; }
+}
