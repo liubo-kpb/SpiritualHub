298c3c4c397605f54d2c0ac5589090dfef1b2320
PREDATOR-LAPTOP\narod <lubomirtoshev96@gmail.com>
2024-02-18 00:21:53 +0200

    Export controller validation logic to ValidationService

---
+++ SpiritualHub.Services/Interfaces/IValidationService.cs
@@ -0,0 +1,13 @@
+﻿namespace SpiritualHub.Services.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+public interface IValidationService
+{
+    string AuthorId { get; set; }
+
+    Task<IActionResult?> IsValidAction(bool entityExists, bool isUserAdmin, string userId, string entityName, ITempDataDictionary tempData);
+
+    Task<IActionResult?> CanUseModifyActionAsync(string userId, ITempDataDictionary tempData);
+}
---
+++ SpiritualHub.Services/ValidationService.cs
@@ -0,0 +1,81 @@
+﻿namespace SpiritualHub.Services;
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+using Interfaces;
+using Data.Models;
+
+
+using static Common.NotificationMessagesConstants;
+using static Common.ErrorMessagesConstants;
+
+public class ValidationService : ControllerBase, IValidationService
+{
+    protected readonly IAuthorService _authorService;
+    protected readonly ICategoryService _categoryService;
+    protected readonly IPublisherService _publisherService;
+
+    public ValidationService(
+        IAuthorService authorService,
+        ICategoryService categoryService,
+        IPublisherService publisherService)
+    {
+        _authorService = authorService;
+        _categoryService = categoryService;
+        _publisherService = publisherService;
+    }
+
+    public string AuthorId { get; set; } = null!;
+
+    public async Task<IActionResult?> IsValidAction(bool entityExists, bool isUserAdmin, string userId, string entityName, ITempDataDictionary tempData)
+    {
+        if (!entityExists)
+        {
+            tempData[ErrorMessage] = string.Format(NoEntityFoundErrorMessage, entityName);
+
+            return RedirectToAction("All");
+        }
+
+        if (isUserAdmin)
+        {
+            return null!;
+        }
+
+        return await CanUseModifyActionAsync(userId, tempData);
+    }
+
+    public async Task<IActionResult?> CanUseModifyActionAsync(string userId, ITempDataDictionary tempData)
+    {
+        var result = await IsUserPublisherAsync(userId, tempData);
+
+        return result ?? await IsUserConnectedPublisherAsync(userId, tempData);
+    }
+
+    private async Task<IActionResult?> IsUserPublisherAsync(string userId, ITempDataDictionary tempData)
+    {
+        bool isPublisher = await _publisherService.ExistsByUserIdAsync(userId);
+        if (!isPublisher)
+        {
+            tempData[ErrorMessage] = NotAPublisherErrorMessage;
+
+            return RedirectToAction("Become", nameof(Publisher));
+        }
+
+        return null!;
+    }
+
+    private async Task<IActionResult?> IsUserConnectedPublisherAsync(string userId, ITempDataDictionary tempData)
+    {
+        bool isConnectedPublisher = await _publisherService.IsConnectedToAuthorByUserId(userId, AuthorId);
+        if (!isConnectedPublisher)
+        {
+            tempData[ErrorMessage] = NotAConnectedPublisherErrorMessage;
+
+            return RedirectToAction("Details", nameof(Author), new { id = AuthorId});
+        }
+
+        return null!;
+    }
+}
