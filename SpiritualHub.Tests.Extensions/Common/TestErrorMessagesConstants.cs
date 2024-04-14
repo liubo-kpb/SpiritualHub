2d0583d11df7f8c3c55a1e019bfc553d518d331f
PREDATOR-LAPTOP\narod <lubomirtoshev96@gmail.com>
2024-04-14 16:50:31 +0300

    Create Edit tests.

--- SpiritualHub.Tests.Extensions/Common/TestErrorMessagesConstants.cs
+++ SpiritualHub.Tests.Extensions/Common/TestErrorMessagesConstants.cs
@@ -3,4 +3,5 @@
 public static class TestErrorMessagesConstants
 {
     public const string NotDbUpdateExceptionErrorMessage = "Database did not throw an exception when updating.";
+    public const string NoNullReferenceExceptionErrorMessage = "Method did not throw a NullReferenceException.";
 }
---
+++ SpiritualHub.Tests/Service/BusinessService/CategoryService/EditTests.cs
@@ -0,0 +1,93 @@
+﻿namespace SpiritualHub.Tests.Service.BusinessService.CategoryService;
+
+using System;
+
+using Moq;
+
+using Data.Models;
+
+using static Extensions.Common.TestErrorMessagesConstants;
+
+public class EditTests : MockConfiguration
+{
+    [Test]
+    public async Task WhenSuccess()
+    {
+        // Arrange
+        int id = 1;
+        var category = new Category { Id = id, Name = "Old Name" };
+        string newName = "New name";
+
+        _categoryRepositoryMock.Setup(x => x.GetSingleByIdAsync(It.Is<string>(x => x == id.ToString()))).Returns(Task.FromResult(category)!);
+
+        // Act
+        await _categoryService.EditAsync(id, newName);
+
+        // Assert
+        Assert.That(category.Name, Is.EqualTo(newName));
+        _categoryRepositoryMock.Verify(x => x.GetSingleByIdAsync(It.Is<string>(x => x == id.ToString())), Times.Once);
+        _categoryRepositoryMock.Verify(x => x.Update(It.Is<Category>(x => x.Equals(category))), Times.Once);
+        _categoryRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+    }
+
+    [Test]
+    public void WithWrongId_ThrowsNullReferenceException()
+    {
+        // Arrange
+        int invalidId = 0;
+        string newName = "New name";
+
+        _categoryRepositoryMock.Setup(x => x.GetSingleByIdAsync(It.Is<string>(x => x == invalidId.ToString()))).ReturnsAsync((Category) null!);
+
+        // Act & Assert
+        Assert.ThrowsAsync<NullReferenceException>(async () => await _categoryService.EditAsync(invalidId, newName), NoNullReferenceExceptionErrorMessage);
+    }
+
+    [Test]
+    public async Task WithWrongId_MethodCallTest()
+    {
+        // Arrange
+        int invalidId = 0;
+        string newName = "New name";
+
+        _categoryRepositoryMock.Setup(x => x.GetSingleByIdAsync(It.Is<string>(x => x == invalidId.ToString()))).ReturnsAsync((Category) null!);
+
+        // Act
+        try
+        {
+            await _categoryService.EditAsync(invalidId, newName);
+        }
+        // Assert
+        catch (Exception)
+        {
+            _categoryRepositoryMock.Verify(x => x.GetSingleByIdAsync(It.Is<string>(x => x == invalidId.ToString())), Times.Once);
+            _categoryRepositoryMock.Verify(x => x.Update(It.IsAny<Category>()), Times.Never);
+            _categoryRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Never);
+
+            return;
+        }
+
+        Assert.Fail(NoNullReferenceExceptionErrorMessage);
+    }
+
+    [Test]
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("      ")]
+    public async Task WithInvalidName(string newName)
+    {
+        // Arrange
+        int id = 1;
+        var category = new Category { Id = id, Name = "Old Name" };
+        _categoryRepositoryMock.Setup(x => x.GetSingleByIdAsync(It.Is<string>(x => x == id.ToString()))).Returns(Task.FromResult(category)!);
+
+        // Act
+        await _categoryService.EditAsync(id, newName);
+
+        // Assert
+        Assert.That(category.Name, !Is.EqualTo(newName));
+        _categoryRepositoryMock.Verify(x => x.GetSingleByIdAsync(It.Is<string>(x => x == id.ToString())), Times.Never);
+        _categoryRepositoryMock.Verify(x => x.Update(It.IsAny<Category>()), Times.Never);
+        _categoryRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Never);
+    }
+}
