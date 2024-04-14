e269221f0b7d51c77616da958b7a9a55427c8e5d
PREDATOR-LAPTOP\narod <lubomirtoshev96@gmail.com>
2024-04-14 16:49:41 +0300

    Abstract Mocking configuration to a base class.

---
+++ SpiritualHub.Tests/Service/BusinessService/CategoryService/DeleteTests.cs
@@ -0,0 +1,49 @@
+﻿namespace SpiritualHub.Tests.Service.BusinessService.CategoryService;
+
+using Moq;
+
+using Data.Models;
+
+public class DeleteTests : MockConfiguration
+{
+    [Test]
+    public async Task WhenSuccess()
+    {
+        // Arrange
+        string id = "1";
+        var category = new Category()
+        {
+            Id = 1,
+            Name = "Test"
+        };
+
+        _categoryRepositoryMock.Setup(x => x.GetSingleByIdAsync(It.Is<string>(x => x == id))).Returns(Task.FromResult(category)!);
+        _categoryRepositoryMock.Setup(x => x.Delete(It.Is<Category>(x => x.Equals(category))));
+
+        // Act
+        await _categoryService.DeleteAsync(id);
+
+        // Assert
+        _categoryRepositoryMock.Verify(x => x.GetSingleByIdAsync(It.Is<string>(x => x == id)), Times.Once);
+        _categoryRepositoryMock.Verify(x => x.Delete(It.Is<Category>(x => x.Equals(category))));
+        _categoryRepositoryMock.Verify(x => x.Delete(It.IsAny<Category>()), Times.Once);
+        _categoryRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+    }
+
+    [Test]
+    public async Task WithWrongId()
+    {
+        // Arrange
+        string invalidId = "invalidId";
+
+        _categoryRepositoryMock.Setup(x => x.GetSingleByIdAsync(It.Is<string>(x => x == invalidId))).ReturnsAsync((Category) null!);
+
+        // Act
+        await _categoryService.DeleteAsync(invalidId);
+
+        // Assert
+        _categoryRepositoryMock.Verify(x => x.GetSingleByIdAsync(It.Is<string>(x => x == invalidId)), Times.Once);
+        _categoryRepositoryMock.Verify(x => x.Delete(It.IsAny<Category>()), Times.Once);
+        _categoryRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+    }
+}
---
+++ SpiritualHub.Tests/Service/BusinessService/CategoryService/MockConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace SpiritualHub.Tests.Service.BusinessService.CategoryService;
+
+using AutoMapper;
+using Moq;
+
+using Data.Models;
+using Data.Repository.Interfaces;
+using Services;
+using Services.Interfaces;
+
+public class MockConfiguration
+{
+    protected ICategoryService _categoryService;
+    protected Mock<IDeletableRepository<Category>> _categoryRepositoryMock;
+    protected Mock<IMapper> _mapperMock;
+
+    [SetUp]
+    public void Setup()
+    {
+        _categoryRepositoryMock = new Mock<IDeletableRepository<Category>>();
+        _mapperMock = new Mock<IMapper>();
+
+        _categoryService = new CategoryService(_categoryRepositoryMock.Object, _mapperMock.Object);
+    }
+}
