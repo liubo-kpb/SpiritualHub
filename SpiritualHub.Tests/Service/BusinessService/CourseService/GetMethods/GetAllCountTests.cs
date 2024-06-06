5a1919c2061b757e5e822d71e8c16638b0209b8c
PREDATOR-LAPTOP\narod <lubomirtoshev96@gmail.com>
2024-06-06 14:27:16 +0300

    Add GetAllCount for course tests

---
+++ SpiritualHub.Tests/Service/BusinessService/CourseService/GetMethods/GetAllCountTests.cs
@@ -0,0 +1,22 @@
+﻿namespace SpiritualHub.Tests.Service.BusinessService.CourseService.GetMethods;
+
+using MockQueryable.Moq;
+
+public class GetAllCountTests : MockConfiguration
+{
+    [Test]
+    public async Task WhenSuccess()
+    {
+        // Arrange
+        int expeceted = _courses.Count;
+
+        _courseRepositoryMock.Setup(x => x.AllAsNoTracking()).Returns(_courses.AsQueryable().BuildMock());
+
+        // Act
+        var result = await _courseService.GetAllCountAsync();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expeceted));
+        _courseRepositoryMock.Verify(x => x.AllAsNoTracking());
+    }
+}
