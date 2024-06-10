73147298842ca0b14680bfb3bc0d74f9b38eacc1
PREDATOR-LAPTOP\narod <lubomirtoshev96@gmail.com>
2024-06-10 22:51:19 +0300

    Add Get neibhoring modules' IDs.

---
+++ SpiritualHub.Tests/Service/BusinessService/ModuleService/GetMethods/GetNextModuleTests.cs
@@ -0,0 +1,78 @@
+﻿namespace SpiritualHub.Tests.Service.BusinessService.ModuleService.GetMethods;
+
+using Client.ViewModels.Module;
+using Services.Mappings;
+using Data.Configuration.Seed;
+
+public class GetNextModuleTests : MockConfiguration
+{
+    [Test]
+    public void WhenModuleIsActive()
+    {
+        // Arrange
+        var course = new SeedCourseConfiguration().GenerateEntities().First();
+        course.Modules = _modules.Where(m => m.CourseID == course.Id).ToList();
+
+        var module = course.Modules.First(m => m.Number == 1);
+        var moduleId = module.Id.ToString();
+
+        var moduleViewModel = _mapper.Map<ModuleDetailsViewModule>(module);
+        _mapper.MapListToViewModel(course.Modules, moduleViewModel.Modules);
+
+        var expectedId = course.Modules.First(m => m.Number == 2).Id.ToString();
+
+        // Act
+        var result = _moduleService.GetNextModuleId(moduleViewModel, false);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedId));
+    }
+
+    [Test]
+    public void WhenSecondModuleIsInactive()
+    {
+        // Arrange
+        var course = new SeedCourseConfiguration().GenerateEntities().First();
+        course.Modules = _modules.Where(m => m.CourseID == course.Id).ToList();
+
+        var module = course.Modules.First(m => m.Number == 1);
+        var moduleId = module.Id.ToString();
+
+        course.Modules.First(m => m.Number == 2).IsActive = false;
+
+        var moduleViewModel = _mapper.Map<ModuleDetailsViewModule>(module);
+        _mapper.MapListToViewModel(course.Modules, moduleViewModel.Modules);
+
+        var expectedId = course.Modules.First(m => m.Number == 3).Id.ToString();
+
+        // Act
+        var result = _moduleService.GetNextModuleId(moduleViewModel, false);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedId));
+    }
+
+    [Test]
+    public void WhenSeconModuleIsInactive_ButCanAccess()
+    {
+        // Arrange
+        var course = new SeedCourseConfiguration().GenerateEntities().First();
+        course.Modules = _modules.Where(m => m.CourseID == course.Id).ToList();
+
+        var module = course.Modules.First(m => m.Number == 1);
+        var moduleId = module.Id.ToString();
+
+        course.Modules.First(m => m.Number == 2).IsActive = false;
+
+        var moduleViewModel = _mapper.Map<ModuleDetailsViewModule>(module);
+        _mapper.MapListToViewModel(course.Modules, moduleViewModel.Modules);
+
+        var expectedId = course.Modules.First(m => m.Number == 2).Id.ToString();
+
+        // Act
+        var result = _moduleService.GetNextModuleId(moduleViewModel, true);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedId));
+    }
+}
