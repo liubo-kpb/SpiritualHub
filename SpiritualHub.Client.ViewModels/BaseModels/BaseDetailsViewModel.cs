e807b4f7b93888085e817c9a50057a4ea738bf2f
Lyubomir Toshev <narodnobile@outlook.com>
2024-07-05 14:53:43 +0300

    Override Equals method

--- SpiritualHub.Client.ViewModels/BaseModels/BaseDetailsViewModel.cs
+++ SpiritualHub.Client.ViewModels/BaseModels/BaseDetailsViewModel.cs
@@ -3,4 +3,14 @@
 public class BaseDetailsViewModel
 {
     public string Id { get; set; } = null!;
+
+    public override bool Equals(object? obj)
+    {
+        return (obj is BaseDetailsViewModel other && this.Id == other.Id)  || base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
 }
